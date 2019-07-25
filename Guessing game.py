ef2f6a0e35233c92308e0cdda5ac82dedf58e79e
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-25 19:21:58 +0200

    creating different output depending on the input

--- Guessing game.py
+++ Guessing game.py
@@ -1,7 +1,15 @@
 secret_word = 'python'
 your_guess = ''
+guess_count = 0
+guess_limit = 5
 
 #while loop to ask for a word until the input is right
-while your_guess != secret_word:
+while your_guess != secret_word and guess_count < guess_limit:
     your_guess = input('Enter your guess: ')
-print('you win!')
+    guess_count += 1                            #count the number of inputs
+
+# different output depending on the right or false input
+if your_guess == secret_word:
+    print('you win!')
+else:
+    print('out of guesses, you lose!')
